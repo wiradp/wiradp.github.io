bddbf538508675aa9b5e28f12cc7fc1dabae71bd
Wiradp <wiradp@yahoo.com>
2023-01-27 22:13:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1320px',
+    }
+  },
+  plugins: [],
+}
