70a4dd0cd49a7c0bd32ec95cf522323f2a1ab372
Wiradp <wiradp@yahoo.com>
2023-01-30 12:20:55 +0700

    add bg fix in html & tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
       },
+      backgroundImage: {
+        'bghome': "url('/dist/img/home-bg3.jpg')",
+      }
     },
     screens: {
       'sm': '640px',
